295dc6c9b80e77f923ffca0218fecde6a1ae25bb
Jakub Baron <jakubaron@gmail.com>
2017-11-08 21:02:51 +0000

    Adding 2 mock events for testing the future views

---
+++ mock_events.jsx
@@ -0,0 +1,19 @@
+export default [
+  {
+    "id": 1,
+    "name": "Mock Event 1",
+    "date": new Date(2017, 11, 11, 18, 00),
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "imageSource": "http://www.pixagen.com/storage/b3s/NOPE.jpg",
+    "maxAttendance": 15
+  },
+  {
+    "id": 2,
+    "name": "Mock Event 2",
+    "date": new Date(2017, 11, 12, 17, 00),
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "imageSource": "http://www.pixagen.com/storage/b3s/NOPE.jpg",
+    "maxAttendance": 12
+  },
+];
+
