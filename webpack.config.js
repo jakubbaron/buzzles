f55f0d2b83a80447e8d0448322db90f2b3afdb9c
Jakub Baron <jakubaron@gmail.com>
2017-11-12 11:27:15 +0000

    Temporarily fixing non existing pages when going straight to the link

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-    entry: './index.js',
-    output: {
-      path: __dirname,
-      filename: 'bundle.js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.jsx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/
-        }
-      ]
-    }
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  //This seems a bit hacky, might need some investigation
+  //https://stackoverflow.com/questions/43209666/react-router-v4-cannot-get-url
+  devServer: {
+    historyApiFallback: true,
   }
+}
